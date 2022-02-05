3b6d41a062ee174bc5568621f91552b3f51dbc3a
Bruno L. Carli <brunolcarli@gmail.com>
2022-02-05 15:08:46 -0300

    ADD background server to keep running on the replit cloud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
-from hilda_garde.settings import TOKEN, SETTINGS_MODULE, __version__
+from hilda_garde.settings import TOKEN, __version__
 from core.commands import client
+from core.keep_alive import keep_alive
 
 
 logging.basicConfig(level='INFO')
@@ -13,5 +14,5 @@ if __name__ == '__main__':
         '''
     )
     log.info('Running Hilda Garde version: %s\n', __version__)
-
+    keep_alive()
     client.run(TOKEN)
