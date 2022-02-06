5c590ef1a3ece0f7e4d0d9c76f2114a98385b771
Bruno L. Carli <brunolcarli@gmail.com>
2022-02-05 21:24:30 -0300

    CHANGE version

--- hilda_garde/settings.py
+++ hilda_garde/settings.py
@@ -3,7 +3,7 @@ Main global settings and configs.
 """
 from decouple import config
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 TOKEN = config('TOKEN', '')
 SETTINGS_MODULE = config('SETTINGS_MODULE', 'common')
